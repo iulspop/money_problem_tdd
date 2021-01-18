fcbca9d888d489915db9286485fa311715e318ab
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-18 11:17:12 -0500

    add project and solution up to half-way through book

---
+++ src/test/java/MoneyTest.java
@@ -0,0 +1,60 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+  @Test
+  public void testMultiplication() {
+    Money five = Money.dollar(5);
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
+  }
+
+  @Test
+  public void testEquality() {
+    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+  }
+  
+  @Test
+  public void testEqualityWithTwoCurrencies() {
+    assertFalse(Money.dollar(5).equals(Money.franc(5)));
+  }
+
+  @Test
+  public void testDifferentClassEquality() {
+    assertTrue(new Money(10, "CHF").equals(Money.franc(10)));
+  }
+
+  @Test
+  public void testFrancMultiplication() {
+    Money five = Money.franc(5);
+    assertEquals(Money.franc(10), five.times(2));
+    assertEquals(Money.franc(15), five.times(3));
+  }
+
+  @Test
+  public void testCurrency() {
+    assertEquals("USD", Money.dollar(1).currency());
+    assertEquals("CHF", Money.franc(1).currency());
+  }
+
+  @Test
+  public void testSimpleAddition() {
+    Money five = Money.dollar(5);
+    Expression sum = five.plus(five);
+    Bank bank = new Bank();
+    Money reduced = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(10), reduced);
+  }
+  
+  public void testPlusReturnsSum() {
+    Money five = Money.dollar(5);
+    Expression result = five.plus(five);
+    Sum sum = (Sum) result;
+    assertEquals(five, sum.augend);
+    assertEquals(five, sum.addend);
+  }
+}
